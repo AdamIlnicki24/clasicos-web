2f00edf4a21736c512e8b308b23f8239df12f2c4
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-05-12 10:13:40 +0200

    Create constants

---
+++ src/constants/buttonLabels.ts
@@ -0,0 +1,1 @@
+export const BACK_TO_HOMEPAGE_LABEL = "Wróć do strony głównej";
---
+++ src/constants/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  primaryColor: "#",
+  secondaryColor: "#",
+  accentColor: "#",
+  defaultWhite: "#FFF",
+  defaultBlack: "#000",
+  defaultGreen: "#22c55e",
+  textDanger: "#dc3545",
+};
