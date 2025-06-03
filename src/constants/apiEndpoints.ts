c447acaa435ad9ffafd18ea2662065e21c38a402
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-06-03 13:27:35 +0200

    Add constants with endpoints

---
+++ src/constants/apiEndpoints.ts
@@ -0,0 +1,7 @@
+export const USERS_API_ENDPOINT = "/users";
+export const ME_API_ENDPOINT = `${USERS_API_ENDPOINT}/me`;
+export const PLAYERS_API_ENDPOINT = "/players";
+export const TEAM_API_ENDPOINT = "/team";
+export const COMMENTS_API_ENDPOINT = "/comments";
+export const RECOMMENDATIONS_API_ENDPOINT = "/recommendations";
+export const RECOMMENDATIONS_COUNT_API_ENDPOINT = `${RECOMMENDATIONS_API_ENDPOINT}/count`;
