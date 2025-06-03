3672e12ee837d6f765b69e00d2f10f5c1508c4db
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-06-03 15:24:09 +0200

    Initialize Tanstack Query

--- src/context/AppProviders.tsx
+++ src/context/AppProviders.tsx
@@ -1,17 +1,31 @@
 "use client";
 
 import { HeroUIProvider } from "@heroui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 // TODO: Improve this component
 
 export function AppProviders({ children }: { children: ReactNode }) {
   const router = useRouter();
 
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
   return (
-    <HeroUIProvider locale="pl-PL" navigate={router.push}>
-      {children}
-    </HeroUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <HeroUIProvider locale="pl-PL" navigate={router.push}>
+        {children}
+      </HeroUIProvider>
+    </QueryClientProvider>
   );
 }
