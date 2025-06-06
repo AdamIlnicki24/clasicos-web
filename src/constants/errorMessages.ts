efa7260d174ebf4ee610532553c37980d98c17a7
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-06-06 14:48:55 +0200

    Prepare form schema and hook

--- src/constants/errorMessages.ts
+++ src/constants/errorMessages.ts
@@ -1,3 +1,10 @@
+import {
+  DEFENDERS_LENGTH,
+  FORWARDS_LENGTH,
+  GOALKEEPERS_LENGTH,
+  MIDFIELDERS_LENGTH,
+} from "./lengths";
+
 export const PAGE_DOES_NOT_EXIST = "Ta strona nie istnieje.";
 export const SOMETHING_WENT_WRONG = "Coś poszło nie tak.";
 
@@ -5,3 +12,10 @@ export const EMAIL_ERROR_MESSAGE = "Podany adres e-mail jest nieprawidłowy.";
 export const REQUIRED = "To pole jest obowiązkowe.";
 
 export const MAX_LENGTH_ERROR_MESSAGE = "W to pole można wpisać maksymalnie";
+
+export const INVALID_UUID_FORMAT = "Nieprawidłowy format UUID.";
+
+export const GOALKEEPERS_LENGTH_ERROR_MESSAGE = `Musisz wybrać dokładnie ${GOALKEEPERS_LENGTH} bramkarza.`;
+export const DEFENDERS_LENGTH_ERROR_MESSAGE = `Musisz wybrać dokładnie ${DEFENDERS_LENGTH} obrońców.`;
+export const MIDFIELDERS_LENGTH_ERROR_MESSAGE = `Musisz wybrać dokładnie ${MIDFIELDERS_LENGTH} pomocników.`;
+export const FORWARDS_LENGTH_ERROR_MESSAGE = `Musisz wybrać dokładnie ${FORWARDS_LENGTH} napastników.`;
--- src/constants/lengths.ts
+++ src/constants/lengths.ts
@@ -5,3 +5,8 @@ export const PLAYER_NAME_MAX_LENGTH = 127;
 export const PLAYER_SURNAME_MAX_LENGTH = 127;
 export const PLAYER_POSITION_MAX_LENGTH = 10;
 export const PLAYER_NATIONALITY_MAX_LENGTH = 127;
+
+export const GOALKEEPERS_LENGTH = 1;
+export const DEFENDERS_LENGTH = 4;
+export const MIDFIELDERS_LENGTH = 3;
+export const FORWARDS_LENGTH = 3;
