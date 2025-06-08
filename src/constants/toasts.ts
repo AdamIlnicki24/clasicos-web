fdf3ba190f905fd768c61f7d697d9fde32a68721
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-06-08 15:40:46 +0200

    Add toasts

--- src/constants/toasts.ts
+++ src/constants/toasts.ts
@@ -3,3 +3,7 @@ export const LOG_IN_INVALID_CREDENTIALS_TOAST =
   "Nieprawidłowy login lub hasło.";
 export const LOG_IN_ERROR_TOAST = "Podczas logowania do serwisu wystąpił błąd.";
 export const PLAYER_HAS_BEEN_CREATED_TOAST = "Poprawnie utworzono piłkarza.";
+export const PLAYER_HAS_BEEN_UPDATED_TOAST = "Poprawnie edytowano piłkarza.";
+export const TEAM_HAS_BEEN_CREATED_TOAST = "Poprawnie utworzono drużynę.";
+export const COMMENT_HAS_BEEN_CREATED_TOAST = "Poprawnie dodano komentarz.";
+export const TEAM_HAS_BEEN_UPDATED_TOAST = "Poprawnie edytowano drużynę.";
