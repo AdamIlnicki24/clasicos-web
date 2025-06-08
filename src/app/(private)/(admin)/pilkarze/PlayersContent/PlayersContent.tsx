d5134f5965a14314e4dd1ac73beb8a08887be63e
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-06-08 13:31:54 +0200

    Add todo

--- src/app/(private)/(admin)/pilkarze/PlayersContent/PlayersContent.tsx
+++ src/app/(private)/(admin)/pilkarze/PlayersContent/PlayersContent.tsx
@@ -1,5 +1,7 @@
 import { CreatePlayerForm } from "@/components/forms/admin/players/CreatePlayerForm/CreatePlayerForm";
 
+// TODO: Think abouth switch statement with 4 cases while getting any resource
+
 export function PlayersContent() {
   return (
     <div className="grid min-h-svh place-items-center">
