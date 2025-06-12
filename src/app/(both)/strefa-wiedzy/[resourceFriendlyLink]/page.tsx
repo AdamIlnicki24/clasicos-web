11170a421a598f189b4c4845c72701eb5debb566
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-06-12 08:45:28 +0200

    Install tailwind 3.4.1

--- src/app/(both)/strefa-wiedzy/[resourceFriendlyLink]/page.tsx
+++ src/app/(both)/strefa-wiedzy/[resourceFriendlyLink]/page.tsx
@@ -1,11 +1,9 @@
-import { Button, Input } from "@heroui/react";
 import { ArticlesContent } from "./ArticlesContent/ArticlesContent";
 
 export default function Page() {
   return (
     <>
       <h1>To jest dynamiczny Page z artykułem</h1>
-      <Input color="black" />
       <ArticlesContent />
     </>
   );
