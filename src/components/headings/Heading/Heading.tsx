15514d790b6a30cec42d58ea7a8589ff271c5828
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-06-13 10:10:47 +0200

    Create heading component

---
+++ src/components/headings/Heading/Heading.tsx
@@ -0,0 +1,47 @@
+interface HeadingProps {
+  title: string;
+  HeadingTag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  size?: "xs" | "sm" | "md" | "lg" | "xl";
+  color?: "defaultWhite" | "defaultBlack";
+  weight?: "normal" | "bold";
+}
+
+export function Heading({
+  title,
+  HeadingTag,
+  size = "lg",
+  color = "defaultWhite",
+  weight = "bold",
+}: HeadingProps) {
+  let textSize: string;
+  switch (size) {
+    case "xs":
+      textSize = "text-[1.2rem]";
+      break;
+    case "sm":
+      textSize = "text-[1.5rem]";
+      break;
+    case "md":
+      textSize = "text-[2rem]";
+      break;
+    case "lg":
+      textSize = "text-[2.5rem]";
+      break;
+    case "xl":
+    default:
+      textSize = "text-[1rem]";
+  }
+
+  const textColor =
+    color === "defaultWhite" ? "text-defaultWhite" : "text-defaultBlack";
+
+  const fontWeight = weight === "bold" ? "font-bold" : "font-normal";
+
+  return (
+    <HeadingTag
+      className={`tracking-wide ${textSize} ${textColor} ${fontWeight}`}
+    >
+      {title}
+    </HeadingTag>
+  );
+}
