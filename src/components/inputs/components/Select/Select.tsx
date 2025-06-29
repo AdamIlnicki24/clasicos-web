5b17cff5f522a2656f1283ad03449f2a0dfaf651
AdamIlnicki24 <adam.ilnicki@essateam.pl>
2025-06-29 10:59:26 +0200

    Improve defenders select

--- src/components/inputs/components/Select/Select.tsx
+++ src/components/inputs/components/Select/Select.tsx
@@ -3,13 +3,26 @@ import {
   SelectProps,
 } from "@heroui/react";
 
+// TODO: Remove unnecessary classNames
+
 export function Select({ ...properties }: SelectProps) {
   return (
     <HeroUISelect
       classNames={{
+        base: "",
+        description: "",
+        helperWrapper: "",
+        innerWrapper: "",
+        listboxWrapper: "",
+        mainWrapper: "",
+        popoverContent: "",
+        selectorIcon: "",
+        spinner: "",
         errorMessage: "text-[1rem]",
         label: "text-[1rem]",
         trigger: "border-2 border-primaryColor",
+        listbox: "text-defaultBlack",
+        value: "text-defaultBlack",
       }}
       {...properties}
     >
--- src/components/inputs/selects/DefendersSelect/DefendersSelect.tsx
+++ src/components/inputs/selects/DefendersSelect/DefendersSelect.tsx
@@ -4,25 +4,31 @@ import { Player } from "@/types/player";
 import { useGetPlayers } from "@/hooks/api/players/useGetPlayers";
 import { useFormikContext } from "formik";
 import { DEFENDERS_LABEL } from "@/constants/labels";
+import { DEFENDERS_LENGTH } from "@/constants/lengths";
 
 export function DefendersSelect() {
   const { data: defenders = [] } = useGetPlayers("Defender");
 
-  const { handleChange, values, errors, touched } = useFormikContext<{
-    defenders: string[];
-  }>();
+  const { handleChange, values, errors, touched, setFieldValue } =
+    useFormikContext<{
+      defenders: string[];
+    }>();
 
   return (
     <Select
       name="defenders"
       label={DEFENDERS_LABEL}
       labelPlacement="outside"
-      isInvalid={touched.defenders && !!errors.defenders}
+      isInvalid={!!(touched.defenders && errors.defenders)}
       errorMessage={touched.defenders && errors.defenders}
       isRequired
-      onChange={handleChange}
-      selectedKeys={values.defenders || []}
-      multiple
+      onSelectionChange={(keys) => {
+        const defendersArray = Array.from(keys) as string[];
+        if (defendersArray.length <= DEFENDERS_LENGTH)
+          setFieldValue("defenders", defendersArray);
+      }}
+      selectedKeys={new Set(values.defenders)}
+      selectionMode="multiple"
     >
       {defenders.map((defender: Player) => (
         <SelectItem
