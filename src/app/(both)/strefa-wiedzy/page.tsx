3774e4693368686aa8c5c885dd7cd36f78072558
AdamIlnicki24 <137420588+AdamIlnicki24@users.noreply.github.com>
2025-07-17 12:08:38 +0200

    Move folder

--- src/app/(both)/strefa-wiedzy/page.tsx
+++ src/app/(both)/strefa-wiedzy/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import { KnowledgeZoneContent } from "./components/KnowledgeZoneContent/KnowledgeZoneContent";
+import { KnowledgeZoneContent } from "./KnowledgeZoneContent/KnowledgeZoneContent";
 import { KNOWLEDGE_ZONE_PAGE_META_DESCRIPTION } from "@/constants/metaDescriptions";
 import { KNOWLEDGE_ZONE_PAGE_HTML_TITLE } from "@/constants/htmlTitles";
 import { ContentContainer } from "@/components/containers/ContentContainer/ContentContainer";
